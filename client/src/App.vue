d2568e073c9e822a6de2ef6f3616df97a967f88d
KameliaTag <taguemountkamelia094@gmail.com>
2022-12-14 16:08:43 +0100

    add client vue and tailwind

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+
+  <div class="wrapper">
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
