d665fa50cee120ae24ba8fa9bb611f85bdff6791
KameliaTag <taguemountkamelia094@gmail.com>
2023-01-19 12:58:08 +0100

    test CI connection BD

--- api/migrations/Version20230119114351.php
+++ api/migrations/Version20230119114351.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230119073532 extends AbstractMigration
+final class Version20230119114351 extends AbstractMigration
 {
     public function getDescription(): string
     {
