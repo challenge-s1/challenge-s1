14d3075ed9a66144c87e968faf667af8bbcf5815
KameliaTag <taguemountkamelia094@gmail.com>
2023-01-19 17:20:37 +0100

    update show message

--- client/src/views/PasswordForgot.vue
+++ client/src/views/PasswordForgot.vue
@@ -3,12 +3,13 @@ import { ref } from 'vue';
 import Modal from '../components/Modal.vue';
 import axios from 'axios';
 
+const showMessage = ref(false);
 
 const open = ref(true);
 const email = ref('');
 
 const handleOpen = () => {
-  open.value = !open.value;
+    open.value = !open.value;
 };
 
 const handleSubmit = () => {
@@ -16,12 +17,14 @@ const handleSubmit = () => {
     handleOpen();
     axios.post('https://localhost/users/reset-password', { email: email.value })
         .then((response) => {
-        console.log(response);
+            console.log(response);
+
         })
         .catch((error) => {
-        console.log(error);
+            console.log(error);
         });
-  
+    showMessage.value = true;
+
 };
 
 
@@ -30,11 +33,11 @@ const handleSubmit = () => {
 
 <template>
 
-<!-- 
+    <!-- 
     <div class="mt-20 bg-emerald-200" v-if="!open">
        
     </div> -->
-    <div class="container px-6 mx-auto" v-if="!open">
+    <div class="container px-6 mx-auto" v-if="showMessage">
         <div class="flex  text-center md:text-left md:flex-row h-screen justify-evenly md:items-center">
 
             <div class="bg-white p-10 flex flex-col shadow-xl rounded-xl">
@@ -43,30 +46,24 @@ const handleSubmit = () => {
 
         </div>
     </div>
-    
+
 
     <Modal :open="open" @close="handleOpen">
         <template #title>
-        <h1 class="text-2xl font-semibold text-gray-700">Reset Password</h1>
+            <h1 class="text-2xl font-semibold text-gray-700">Reset Password</h1>
         </template>
         <form @submit.prevent="handleSubmit">
-                <div class="flex flex-col mt-2">
+            <div class="flex flex-col mt-2">
                 <label class="mb-1 text-sm font-semibold text-gray-600">Email</label>
                 <input
                     class="px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                    type="email"
-                    placeholder="Email"
-                    v-model="email"
-                />
-                </div>
-                <div class="flex items-center justify-between mt-4">
-                <button
-                    type="submit"
-                    class="px-4 py-2 font-semibold text-white bg-blue-500 rounded hover:bg-blue-700"
-                >
+                    type="email" placeholder="Email" v-model="email" />
+            </div>
+            <div class="flex items-center justify-between mt-4">
+                <button type="submit" class="px-4 py-2 font-semibold text-white bg-blue-500 rounded hover:bg-blue-700">
                     Send
                 </button>
-                </div>
+            </div>
         </form>
 
     </Modal>
