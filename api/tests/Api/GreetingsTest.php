4b8ecc850853a213dfe405b13d018d61b7165ddc
KameliaTag <taguemountkamelia094@gmail.com>
2023-01-19 13:49:34 +0100

    add test for login user /test ci phpunit

--- api/tests/Api/GreetingsTest.php
+++ api/tests/Api/GreetingsTest.php
@@ -1,22 +1,50 @@
 <?php
+// tests/AuthenticationTest.php
 
 namespace App\Tests\Api;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\User;
+use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 
-class GreetingsTest extends ApiTestCase
+class UserTese extends ApiTestCase
 {
-    public function testCreateGreeting(): void
-    {
-        static::createClient()->request('POST', '/greetings', ['json' => [
-            'name' => 'Kévin',
-        ]]);
+    use ReloadDatabaseTrait;
 
-        $this->assertResponseStatusCodeSame(201);
-        $this->assertJsonContains([
-            '@context' => '/contexts/Greeting',
-            '@type' => 'Greeting',
-            'name' => 'Kévin',
+    public function testLogin(): void
+    {
+        $client = self::createClient();
+        $container = self::getContainer();
+
+        $user = new User();
+        $user->setEmail('user1@gmail.com');
+        $user->setPassword(
+            $container->get('security.user_password_hasher')->hashPassword($user, 'test')
+        );
+
+        $manager = $container->get('doctrine')->getManager();
+        $manager->persist($user);
+        $manager->flush();
+
+        // retrieve a token
+        $response = $client->request('POST', '/authentication_token', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => 'user1@gmail.com',
+                'password' => 'test',
+            ],
         ]);
+
+        $json = $response->toArray();
+        $this->assertResponseIsSuccessful();
+        $this->assertArrayHasKey('token', $json);
+
+        // test not authorized
+        $client->request('GET', '/greetings');
+        $this->assertResponseStatusCodeSame(401);
+
+        // test authorized
+        $client->request('GET', '/greetings', ['auth_bearer' => $json['token']]);
+        $this->assertResponseIsSuccessful();
     }
 }
