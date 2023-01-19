e4414f7324c4cfb42bfd3ccf32187ba6cdb8a25b
KameliaTag <taguemountkamelia094@gmail.com>
2023-01-19 17:02:46 +0100

    update megration

--- api/migrations/Version20230119154326.php
+++ api/migrations/Version20230119154326.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230119114351 extends AbstractMigration
+final class Version20230119154326 extends AbstractMigration
 {
     public function getDescription(): string
     {
