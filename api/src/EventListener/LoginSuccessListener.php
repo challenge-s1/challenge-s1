543d2b7561a1c4a8bce1bbc89413a3cfa55017a6
Sonia Oussadi <oussadi.sonia96@gmail.com>
2023-02-05 23:34:46 +0100

    get user role

--- api/src/EventListener/LoginSuccessListener.php
+++ api/src/EventListener/LoginSuccessListener.php
@@ -18,6 +18,7 @@ class LoginSuccessListener
         // Add information to user payload
         $payload['user'] = [
             'id' => $user->getId(),
+            'role' => $user->getRoles(),
         ];
         $event->setData($payload);
     }
