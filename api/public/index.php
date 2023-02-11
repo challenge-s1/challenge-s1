eabff7f560f3e85a54a6dc7a8a079630b22c6d4b
naty-hub <natyghanem@hotmail.fr>
2023-02-11 21:22:30 +0100

    retray CorsSubscriber

--- api/public/index.php
+++ api/public/index.php
@@ -2,6 +2,11 @@
 
 use App\Kernel;
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+header("Allow: *");
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
