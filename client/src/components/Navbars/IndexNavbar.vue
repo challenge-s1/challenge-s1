29fa08a9281e138184826f9ea0e3dcac21c96ffc
KameliaTag <taguemountkamelia094@gmail.com>
2023-02-13 21:59:40 +0100

    add more detail

--- client/src/components/Navbars/IndexNavbar.vue
+++ client/src/components/Navbars/IndexNavbar.vue
@@ -149,6 +149,7 @@ const isAdmin = computed({
               </button>
             </router-link>
           </li>
+
           <li class="flex items-center" v-else>
             <button v-if="isAdmin">
               <router-link :to="{ name: 'Dashbord' }"
@@ -158,6 +159,19 @@ const isAdmin = computed({
 
               </router-link>
             </button>
+            <button v-if="store.getters.isLoggedIn">
+              <router-link :to="{ name: 'Cart' }"
+                class="border-white text-black  text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+                type="button">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                  stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                </svg>
+
+              </router-link>
+            </button>
+
             <h1 class="mr-2 font-bold">
               {{ userToken.firstname }} {{ userToken.lastname }}
             </h1>
